783198b16242e1f99d4b87491ae17fbe4f261b13
vaibhav <vaibhavmehta524@gmail.com>
2023-02-18 02:50:52 +0530

    modified to edit contact program

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -8,8 +8,28 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to address book program");
             AddressBook addressBook = new AddressBook();
-            addressBook.CreateContact();
-            addressBook.Display();
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:Create contcat details\n2:Add new contact\n3:Edit contact");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    addressBook.CreateContact();
+                    addressBook.Display();
+                    break;
+                case 2:
+                    addressBook.CreateContact();
+                    addressBook.Display();
+                    break;
+                case 3:
+                    addressBook.CreateContact();
+                    addressBook.EditContract("Shivani");
+                    addressBook.Display();
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
         }
     }
 }
