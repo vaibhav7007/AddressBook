e8caa69d95c4ffcc0803d07d5b27f9616c72b6bb
Vaibhav Mehta <Vaibhavmehta524@gmail.com>
2023-03-31 08:15:16 +0530

    Modified to Sort Contact By Name

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -8,13 +8,37 @@ namespace AddressBook
 {
     public class Contact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PhoneNumber { get; set; }
-        public int Zip { get; set; }
-        public string Email { get; set; }
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string phoneNumber;
+        private int zip;
+        private string email;
+        public Contact()
+        {
+
+        }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int zip, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public int Zip { get => zip; set => zip = value; }
+        public string Email { get => email; set => email = value; }
     }
 }
